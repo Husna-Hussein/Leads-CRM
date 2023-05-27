8ee1c11be30088bf2814ef7d0a67091bf7382728
Husna Hussein <husnakilalo2@gmail.com>
2023-05-27 22:10:29 +0300

    updated templates

--- leads/views.py
+++ leads/views.py
@@ -33,24 +33,43 @@ def lead_create(request):
 
     return render(request, "leads/lead_create.html", context)
 
-
 def lead_update(request, pk):
-   lead = Lead.objects.get(id=pk)
-   form = LeadForm()
-   if request.method == "POST":
-        form = LeadForm(request.POST)
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
         if form.is_valid():
-          first_name = form.cleaned_data['first_name']
-          last_name = form.cleaned_data['last_name']
-          age = form.cleaned_data['age']
-          lead.first_name = first_name
-          lead.last_name = last_name
-          lead.age = age
-          lead.save()
+          form.save()
           return redirect("/leads")
-   context = {
+    context = {
         "form": form,
         "lead": lead
     }
-   return render(request, "leads/lead_update.html", context)
+    return render(request, "leads/lead_update.html", context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
+
+# def lead_update(request, pk):
+#    lead = Lead.objects.get(id=pk)
+#    form = LeadForm()
+#    if request.method == "POST":
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#           first_name = form.cleaned_data['first_name']
+#           last_name = form.cleaned_data['last_name']
+#           age = form.cleaned_data['age']
+#           lead.first_name = first_name
+#           lead.last_name = last_name
+#           lead.age = age
+#           lead.save()
+#           return redirect("/leads")
+#    context = {
+#         "form": form,
+#         "lead": lead
+#     }
+#    return render(request, "leads/lead_update.html", context)
 
