79d796fc2a6ec4df8c061740c44fbb17b85a8b42
Husna Hussein <husnakilalo2@gmail.com>
2023-05-31 16:12:05 +0300

    added agent template

--- agents/urls.py
+++ agents/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AgentListView
+from .views import AgentListView, AgentCreateView
 
 app_name ='agents'
 
 urlpatterns =[
-    path('', AgentListView.as_view, name='agents')
+    path('', AgentListView.as_view(), name='agent-list'),
+    path('create/', AgentCreateView.as_view(), name= 'agent-create'),
 
 ]
--- agents/views.py
+++ agents/views.py
@@ -1,9 +1,11 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.shortcuts import reverse
 from django.views import generic
 from  django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Agent
+from .forms import AgentModelForm
 
 class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name ="agents/agent_list.html"
@@ -11,3 +13,17 @@ class AgentListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return Agent.objects.all()
+    
+class AgentCreateView(LoginRequiredMixin, generic.CreateView):
+    template_name ="agents/agent_create.html"
+    form_class = AgentModelForm
+
+    def get_success_url(self) :
+        return reverse("agents:agent-list")
+
+
+    def form_valid(self, form):
+         agent = form.save(commit =False)
+         agent.organisation = self.request.user.userprofile
+         agent.save()
+         return super(AgentCreateView, self).form_valid(form)
