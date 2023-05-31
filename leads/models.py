f9153e39b405517ad3a6739a4c40a5ba884bf9a0
Husna Hussein <husnakilalo2@gmail.com>
2023-05-31 13:27:46 +0300

    updates

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
@@ -16,7 +23,14 @@ class Lead(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organisation =models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.email
+    
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+        
+post_save.connect(post_user_created_signal, sender=User)
 
