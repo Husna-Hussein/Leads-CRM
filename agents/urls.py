a19f5e49503fadcdf938db797bde98b5e039e3ec
Husna Hussein <husnakilalo2@gmail.com>
2023-05-31 16:56:25 +0300

    worked on updates

--- agents/urls.py
+++ agents/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import AgentListView, AgentCreateView
+from .views import AgentListView, AgentCreateView, AgentDetailView, AgentUpdateView
 
 app_name ='agents'
 
 urlpatterns =[
     path('', AgentListView.as_view(), name='agent-list'),
     path('create/', AgentCreateView.as_view(), name= 'agent-create'),
+    path('<int:pk>/', AgentDetailView.as_view(), name='agent-detail'),
+    path('<int:pk>/update/', AgentUpdateView.as_view(), name='agent-update'),
+
 
 ]
